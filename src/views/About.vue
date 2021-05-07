ec44abc5ae61cfb5e4589cb9abb324d898aade54
xf <508742729@qq.com>
2021-05-07 11:39:01 +0800

    增加码云地址

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,8 +8,9 @@
       <section>
         <h3>说明</h3>
         <p class="intro">作者：lvan</p>
-        <p class="intro">CSDN账号：<lvan-link type="primary" href="https://blog.csdn.net/l508742729" target="_blank">找不到bug的猿</lvan-link></p>
-        <p class="intro">githup项目地址：<lvan-link type="primary" href="https://github.com/lvan521/LvanUI" target="_blank">LvanUI</lvan-link></p>
+        <p class="intro">CSDN 账号：<lvan-link type="primary" href="https://blog.csdn.net/l508742729" target="_blank">找不到bug的猿</lvan-link></p>
+        <p class="intro">githup 项目地址：<lvan-link type="primary" href="https://github.com/lvan521/LvanUI" target="_blank">LvanUI</lvan-link></p>
+        <p class="intro">码云 项目地址：<lvan-link type="primary" href="https://gitee.com/xf727/lvan-ui" target="_blank">Lvan-UI</lvan-link></p>
         <p class="intro">本项目仅作为模仿练习 <lvan-link type="primary" href="https://element-plus.gitee.io/#/zh-CN/component/installation" target="_blank">ElementPlus</lvan-link> 的vue组件封装参考，不用于任何商业性质活动。</p>
       </section>
     </div>
